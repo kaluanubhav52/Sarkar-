c7f671dd5ce7aaf38bd334b0ba20ed9438c9bcdb
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-03-05 00:04:31 +0530

    Refactor datetime usage in commands.py
    Removed redundant datetime import and updated references to use the imported datetime directly.

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,17 +2,16 @@ import os
 import random
 import string
 import asyncio
+from datetime import datetime, timedelta
 from time import time as time_now
 from time import monotonic
-import datetime
 from Script import script
 from hydrogram import Client, filters, enums
 from hydrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import db_count_documents, second_db_count_documents, get_file_details, delete_files
 from database.users_chats_db import db
-from datetime import datetime, timedelta
-from info import IS_PREMIUM, PRE_DAY_AMOUNT, RECEIPT_SEND_USERNAME, URL, BIN_CHANNEL, SECOND_FILES_DATABASE_URL, STICKERS, INDEX_CHANNELS, ADMINS, IS_VERIFY, VERIFY_TUTORIAL, VERIFY_EXPIRE, SHORTLINK_API, SHORTLINK_URL, DELETE_TIME, SUPPORT_LINK, UPDATES_LINK, LOG_CHANNEL, PICS, IS_STREAM, REACTIONS, PM_FILE_DELETE_TIME
-from utils import is_premium, upload_image, get_settings, get_size, is_subscribed, is_check_admin, get_shortlink, get_verify_status, update_verify_status, save_group_settings, temp, get_readable_time, get_wish, get_seconds
+from info import IS_PREMIUM, PRE_DAY_AMOUNT, RECEIPT_SEND_USERNAME, URL, BIN_CHANNEL, SECOND_FILES_DATABASE_URL, STICKERS
+from utils import is_premium, upload_image, get_settings, get_size, is_subscribed, check_admin, get_shortlink, get_verify_status, update_verify_status, get_wish
 
 async def del_stk(s):
     await asyncio.sleep(3)
@@ -48,7 +47,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.NEW_USER_TXT.format(message.from_user.mention, message.from_user.id))
 
     verify_status = await get_verify_status(message.from_user.id)
-    if verify_status['is_verified'] and datetime.datetime.now() > verify_status['expire_time']:
+    if verify_status['is_verified'] and datetime.now() > verify_status['expire_time']:
         await update_verify_status(message.from_user.id, is_verified=False)
 
 
