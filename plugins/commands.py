8bee4add72ba22fec86c1708fac0b661437c2b04
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-03-04 23:37:19 +0530

    Fix datetime usage for expiry time calculation

--- plugins/commands.py
+++ plugins/commands.py
@@ -103,7 +103,7 @@ async def start(client, message):
         verify_status = await get_verify_status(message.from_user.id)
         if verify_status['verify_token'] != token:
             return await message.reply("Your verify token is invalid.")
-        expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=VERIFY_EXPIRE)
+        expiry_time = datetime.now() + timedelta(seconds=VERIFY_EXPIRE)
         await update_verify_status(message.from_user.id, is_verified=True, expire_time=expiry_time)
         if verify_status["link"] == "":
             reply_markup = None
