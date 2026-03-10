d73c83c1389a232a9918387301d8f2e2cf58cef1
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-03-10 16:15:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -125,9 +125,12 @@ async def start(client, message):
         ],[
             InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{temp.U_NAME}?start={mc}")
         ]]
-        await message.reply("You not verified today! Kindly verify now. 🔐", reply_markup=InlineKeyboardMarkup(btn), protect_content=False)
-        await asyncio.sleep(600) 
+    sent_msg = await message.reply("You not verified today! Kindly verify now. 🔐", reply_markup=InlineKeyboardMarkup(btn), protect_content=False)
+    await asyncio.sleep(600) 
+    try:
         await sent_msg.delete()
+    except Exception as e:
+        pass
         return
 
     btn = await is_subscribed(client, message)
